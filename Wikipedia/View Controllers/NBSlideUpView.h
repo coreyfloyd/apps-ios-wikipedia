8f639d7ec585c1b3a7955d76f2061d69aa1afc34
Corey Floyd <cfloyd@wikimedia.org>
2015-04-14 20:19:42 -0400

    Adding Original NBSlideUpView
    Change-Id: I6b58ead5308d67734f8ea38deb8cb316dd6c1e24

---
+++ Wikipedia/View Controllers/NBSlideUpView.h
@@ -0,0 +1,39 @@
+//
+//  CardView.h
+//  gftbk
+//
+//  Created by Neeraj Baid on 3/21/14.
+//  Copyright (c) 2014 Neeraj Baid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol NBSlideUpViewDelegate <NSObject>
+
+- (void)slideUpViewDidAnimateOut:(UIView *)slideUpView;
+- (void)slideUpViewDidAnimateIn:(UIView *)slideUpView;
+- (void)slideUpViewDidAnimateRestore:(UIView *)slideUpView;
+
+@end
+
+@interface NBSlideUpView : UIView <UIAlertViewDelegate>
+
+- (id)initWithSuperview:(UIView *)superview viewableHeight:(CGFloat)viewablePixels;
+- (void)animateOut;
+- (void)animateIn;
+
+@property (nonatomic) CGPoint previousTouch;
+
+@property (nonatomic) CGFloat viewablePixels;
+@property (nonatomic) CGFloat dragMultiplier;
+
+@property (nonatomic) CGFloat springDamping;
+@property (nonatomic) CGFloat initialSpringVelocity;
+@property (nonatomic) CGFloat animateInOutTime;
+
+@property (nonatomic) CGFloat arrowAlpha;
+@property (nonatomic) BOOL superviewIsScrollView;
+@property (nonatomic, strong) UIView *contentView;
+@property (nonatomic, strong) id<NBSlideUpViewDelegate> delegate;
+
+@end
