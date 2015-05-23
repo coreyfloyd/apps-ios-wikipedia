06a06dfabcdbb4e0f232c21ea12eac226a63c2e2
Corey Floyd <cfloyd@wikimedia.org>
2015-05-23 14:51:32 +0200

    Add watch kit app
    Change-Id: I740d58958d2fcad420696324788bbbb08a6992ff

---
+++ Wikipedia WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  Wikipedia WatchKit Extension
+//
+//  Created by Corey Floyd on 5/23/15.
+//  Copyright (c) 2015 Wikimedia Foundation. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
---
+++ Wikipedia WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  Wikipedia WatchKit Extension
+//
+//  Created by Corey Floyd on 5/23/15.
+//  Copyright (c) 2015 Wikimedia Foundation. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
